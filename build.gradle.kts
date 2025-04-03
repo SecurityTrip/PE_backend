32f78a9e190a893f0274ebc191e7f88d4a596901
Ilia Lysov <illigiorno@gmail.com>
2025-04-04 00:21:54 +0400

    Add swagger docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    runtimeOnly("org.postgresql:postgresql")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- src/main/java/com/example/sea_battle/controllers/AuthController.java
+++ src/main/java/com/example/sea_battle/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package com.example.sea_battle.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Контроллер регистрации", description = "Содержит методы для авторизации")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
