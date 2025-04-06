ac71616f8330ecfdc4d6c841df27728aebfb6afa
Ilia Lysov <illigiorno@gmail.com>
2025-04-07 02:05:34 +0400

    Small fix

--- src/main/java/com/example/sea_battle/entities/schemas/User.java
+++ src/main/java/com/example/sea_battle/entities/schemas/User.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
